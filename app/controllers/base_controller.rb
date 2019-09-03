91e11bb4bd7df24d05d9c96c1bbfe512ca394c0f
Sammy Lee <sammy@Sammys-MacBook-Pro.local>
2019-09-03 14:26:33 -0700

    webpage container-fluid, rooted categories, and changed brand

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,4 +1,7 @@
 class BaseController < ApplicationController
   def index
   end
+
+  def category
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: "base#index"
+
+  get "/antiques" => "base#category"
 end
